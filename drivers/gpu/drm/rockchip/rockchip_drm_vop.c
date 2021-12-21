d788df8a748a1b1bca4c4d8e37854e351311f784
bross1_kuo <bross1_kuo@asus.com>
2021-12-21 11:09:06 +0800

    Tinker board: HDMI: VGG804838 HDMI panel porting
    if clock is not equal to request_clock in vop_crtc_mode_valid(),
    
    check below condition
    
    (1)SOC is rk3288C or rk3288CGW
    
    (2)request_clock is 33260 and match EDID
    
    then don't set mode to MODE_CLOCK_RANGE to prevent blank screen,
    
    this solution is WAR
    
    Signed-off-by: bross1_kuo <bross1_kuo@asus.com>
    Change-Id: I1cbcd13cc92cb75f6a1beb4169635cc01b8b85c4

--- drivers/gpu/drm/rockchip/rockchip_drm_vop.c
+++ drivers/gpu/drm/rockchip/rockchip_drm_vop.c
@@ -2645,6 +2645,9 @@ vop_crtc_mode_valid(struct drm_crtc *crtc, const struct drm_display_mode *mode,
 	const struct vop_data *vop_data = vop->data;
 	int request_clock = mode->clock;
 	int clock;
+	bool eve_vgg804838_panel = false;
+
+	eve_vgg804838_panel = detect_eve_vgg804838_panel();
 
 	if (mode->hdisplay > vop_data->max_output.width)
 		return MODE_BAD_HVALUE;
@@ -2662,10 +2665,19 @@ vop_crtc_mode_valid(struct drm_crtc *crtc, const struct drm_display_mode *mode,
 	 * Hdmi or DisplayPort request a Accurate clock.
 	 */
 	if (output_type == DRM_MODE_CONNECTOR_HDMIA ||
-	    output_type == DRM_MODE_CONNECTOR_DisplayPort)
-		if (clock != request_clock)
+	    output_type == DRM_MODE_CONNECTOR_DisplayPort) {
+		if (clock != request_clock) {
+			//MAJOR = 3, MINOR = 0 for rk3288-C
+			//MAJOR = 3, MINOR = 1 for rk3288-CGW
+			if (((VOP_MAJOR(vop->version) == 3) && (VOP_MINOR(vop->version) == 0 || VOP_MINOR(vop->version) == 1))) {
+				if(request_clock == 33260 && eve_vgg804838_panel) {
+					pr_err("%s: don't block pixel clock %d KHz", __func__, request_clock);
+					return MODE_OK;
+				}
+			}
 			return MODE_CLOCK_RANGE;
-
+		}
+	}
 	return MODE_OK;
 }
 
