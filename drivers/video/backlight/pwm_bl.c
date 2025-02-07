bbe1525fcfa5c8d73b51e0402980618f34d0816b
moga_huang <Moga_Huang@asus.com>
2025-02-07 16:10:32 +0800

    panel: lvds: add option to keep pwm low at panel init stage
    Tinker baord 3N: fine tune panel power sequence
    
    if a pin is configured to PWM, this pin will be pulled up in uboot,
    
    and VLED be pulled up in kernel,
    
    but G101EAN02 SPEC say VLED should be pulled up before PWM be pulled up,
    
    call pwm_backlight_power_off in pwm_backlight_probe to pull down PWM to meet power sequence,
    
    but if don't call pwm_backlight_power_on first,
    
    clock will failed to disable, and PWM won't be pulled down,
    
    this is a WAR
    
    Change-Id: I0db01c08a423a689c9ad69142ac9fc8203602c60
    Signed-off-by: bross1_kuo <bross1_kuo@asus.com>
    
    Change-Id: Ib5c6d0734f87f82d2251fc073d2a13e564997f9e
    Signed-off-by: moga_huang <Moga_Huang@asus.com>

--- drivers/video/backlight/pwm_bl.c
+++ drivers/video/backlight/pwm_bl.c
@@ -20,6 +20,7 @@
 #include <linux/slab.h>
 
 unsigned int minimal_brightness = 0;
+bool pwm_pre_pull_low = false;
 static bool bl_quiescent;
 module_param_named(quiescent, bl_quiescent, bool, 0600);
 MODULE_PARM_DESC(quiescent,
@@ -405,6 +406,11 @@ static int pwm_backlight_parse_dt(struct device *dev,
 			&minimal_brightness);
 	}
 
+	if(of_property_read_bool(node, "pwm-pre-pull-low")) {
+		pwm_pre_pull_low = true;
+		pr_err("%s: pwm pre pull low\n", __func__);
+	}
+
 	return 0;
 }
 
@@ -695,6 +701,12 @@ static int pwm_backlight_probe(struct platform_device *pdev)
 	backlight_update_status(bl);
 
 	platform_set_drvdata(pdev, bl);
+
+	if(pwm_pre_pull_low) {
+		pwm_backlight_power_on(pb);
+		pwm_backlight_power_off(pb);
+	}
+
 	return 0;
 
 err_alloc:
